b803e07b79af42330e75615c2a6ce286217d4af4
Quentin HUSLER <qhusler@e1r7p1.42.fr>
2016-12-20 10:38:46 +0100

    maj memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: qhusler <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 13:19:09 by qhusler           #+#    #+#             */
-/*   Updated: 2016/11/07 13:33:11 by qhusler          ###   ########.fr       */
+/*   Updated: 2016/12/20 10:38:19 by qhusler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
 		i++;
 	}
+	((unsigned char*)dst)[i] = '\0';
 	return ((unsigned char*)dst);
 }
